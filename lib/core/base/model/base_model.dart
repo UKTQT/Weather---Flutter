edf9cc77a0c79426409c30ce34eb36afab9615a2
UKTQT <ufukkt2699@hotmail.com>
2022-06-21 20:36:49 +0300

    project base update

--- lib/core/base/model/base_model.dart
+++ lib/core/base/model/base_model.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:havadurumu/core/init/navigation/navigation_service.dart';
+
+abstract class BaseViewModel {
+  BuildContext? baseViewContext;
+  NavigationService navigation = NavigationService.instance;
+
+  void setContext(BuildContext context);
+  void init();
+}
--- lib/core/base/state/base_state.dart
+++ lib/core/base/state/base_state.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-abstract class BaseState<T extends StatefulWidget> extends State<T> {
+abstract class BaseState<T extends StatelessWidget> extends State {
   ThemeData get themeData => Theme.of(context);
   double get screenHeight => MediaQuery.of(context).size.height;
   double get screenWidth => MediaQuery.of(context).size.width;
