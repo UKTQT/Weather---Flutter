512ed95d27fb5c7508bbbbaf53e68d8cb4fdf2f6
UKTQT <ufukkt2699@hotmail.com>
2022-06-22 21:36:16 +0300

    vm update

--- lib/feature/home/home/viewModel/home_view_model.dart
+++ lib/feature/home/home/viewModel/home_view_model.dart
@@ -1,6 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:havadurumu/core/base/model/base_model.dart';
+import 'package:havadurumu/feature/home/home/model/home_model.dart';
+import 'package:havadurumu/feature/home/home/service/home_service.dart';
 import 'package:mobx/mobx.dart';
+
 part 'home_view_model.g.dart';
 
 class HomeViewModel = _HomeViewModelBase with _$HomeViewModel;
 
-abstract class _HomeViewModelBase with Store {}
+abstract class _HomeViewModelBase with Store, BaseViewModel {
+  final HomeService _homeService = HomeService();
+
+  @override
+  void init() {}
+
+  @override
+  void setContext(BuildContext context) => this.baseViewContext = context;
+
+  @observable
+  bool isLoading = false;
+
+  @action
+  void changeLoading() {
+    isLoading = !isLoading;
+  }
+
+  @observable
+  List<HomeResult>? weatherItems = [];
+
+  @action
+  Future<List<HomeResult>?> fetchWeather() async {
+    changeLoading();
+    weatherItems = await _homeService.fetchWeather() ?? [];
+    changeLoading();
+  }
+}
--- lib/feature/home/home/viewModel/home_view_model.g.dart
+++ lib/feature/home/home/viewModel/home_view_model.g.dart
@@ -9,10 +9,65 @@ part of 'home_view_model.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$HomeViewModel on _HomeViewModelBase, Store {
+  late final _$isLoadingAtom =
+      Atom(name: '_HomeViewModelBase.isLoading', context: context);
+
+  @override
+  bool get isLoading {
+    _$isLoadingAtom.reportRead();
+    return super.isLoading;
+  }
+
+  @override
+  set isLoading(bool value) {
+    _$isLoadingAtom.reportWrite(value, super.isLoading, () {
+      super.isLoading = value;
+    });
+  }
+
+  late final _$weatherItemsAtom =
+      Atom(name: '_HomeViewModelBase.weatherItems', context: context);
+
+  @override
+  List<HomeResult>? get weatherItems {
+    _$weatherItemsAtom.reportRead();
+    return super.weatherItems;
+  }
+
+  @override
+  set weatherItems(List<HomeResult>? value) {
+    _$weatherItemsAtom.reportWrite(value, super.weatherItems, () {
+      super.weatherItems = value;
+    });
+  }
+
+  late final _$fetchWeatherAsyncAction =
+      AsyncAction('_HomeViewModelBase.fetchWeather', context: context);
+
+  @override
+  Future<List<HomeResult>?> fetchWeather() {
+    return _$fetchWeatherAsyncAction.run(() => super.fetchWeather());
+  }
+
+  late final _$_HomeViewModelBaseActionController =
+      ActionController(name: '_HomeViewModelBase', context: context);
+
+  @override
+  void changeLoading() {
+    final _$actionInfo = _$_HomeViewModelBaseActionController.startAction(
+        name: '_HomeViewModelBase.changeLoading');
+    try {
+      return super.changeLoading();
+    } finally {
+      _$_HomeViewModelBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+isLoading: ${isLoading},
+weatherItems: ${weatherItems}
     ''';
   }
 }
