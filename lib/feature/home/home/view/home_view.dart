ca5ec1a4cd6eb56574fb2e4781056da47201e4c2
UKTQT <ufukkt2699@hotmail.com>
2022-06-23 21:56:27 +0300

    home view update

--- lib/feature/home/home/view/home_view.dart
+++ lib/feature/home/home/view/home_view.dart
@@ -3,9 +3,8 @@ import 'package:havadurumu/core/base/view/base_view.dart';
 import 'package:havadurumu/feature/home/home/viewModel/home_view_model.dart';
 
 class HomeView extends StatelessWidget {
-  HomeView({Key? key}) : super(key: key);
+  const HomeView({Key? key}) : super(key: key);
 
-  HomeViewModel? viewModel;
   @override
   Widget build(BuildContext context) {
     return BaseView<HomeViewModel>(
@@ -17,7 +16,7 @@ class HomeView extends StatelessWidget {
       onPageBuilder: (BuildContext context, HomeViewModel _homeViewModel) {
         return ListView.builder(
           scrollDirection: Axis.vertical,
-          itemCount: _homeViewModel.weatherItems!.length ?? 1,
+          itemCount: _homeViewModel.weatherItems!.length,
           itemBuilder: (context, index) {
             return Text('${_homeViewModel.weatherItems?[index].day}');
           },
